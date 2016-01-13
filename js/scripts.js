1948fdf1aabbcae8085633aeaba0897dec33e72c
Afton Downey and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-13 14:18:00 -0800

    Add js on click function

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  var elements = ["h1", "p", "img"];
+  elements.forEach(function(element) {
+    $(element).click(function() {
+      alert("This is a " + element);
+    });
+  });
+
+
+});
